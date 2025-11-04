a5b82e74b03a6e1b04122259d7dd45d27a90de98
James Scott <jmscott9@cps.edu>
2025-11-04 11:15:30 -0600

    finished most_constrained

--- a5.py
+++ a5.py
@@ -106,7 +106,20 @@ class Board:
         Returns:
             a tuple of row, column index identifying the most constrained cell
         """
-        pass
+        mini_length = 9
+        mini_col = 0
+        mini_row = 0
+        
+        for row in range(self.size):
+            for col in range(self.size):
+                cell = self.rows[row][col]
+                # print(f"({row}. {col})")
+                if isinstance(cell, list):
+                    if len(cell) < mini_length:
+                        mini_length = len(cell)
+                        mini_row = row
+                        mini_col = col
+        return (mini_row, mini_col)
 
     def failure_test(self) -> bool:
         """Check if we've failed to correctly fill out the puzzle. If we find a cell
